434a79c32c71e279359b6e0cb96bea5206e2d9db
Swami <chandrakant.patil66@gmail.com>
2019-01-31 20:31:37 +0530

    class01 initial commit

--- class01.py
+++ class01.py
@@ -1,1 +1,82 @@
-print("Hello Word")
+
+print('Hello Word')
+print('how are you friends')
+
+
+
+
+#INTEGER DATATYPE
+
+integer_variable1 = 1
+integer_variable2 = 2
+integer_variable3 = 3
+integer_variable4 = 4
+integer_variable5 = 5.55
+
+print(integer_variable1, id(integer_variable1), type(integer_variable1),
+integer_variable2, id(integer_variable1), type(integer_variable1),
+integer_variable3, id(integer_variable1), type(integer_variable1),
+integer_variable4, id(integer_variable1), type(integer_variable1),
+integer_variable5, id(integer_variable1), type(integer_variable1))
+
+
+#FLOAT DATATYPE
+
+float_variable1 = 11.55
+float_variable2 = 28.67353667
+float_variable3 = 3.9
+float_variable4 = 4.0
+float_variable5 = 5
+
+print(float_variable1, id(float_variable1), type(float_variable1),
+float_variable2, id(float_variable2), type(float_variable2),
+float_variable3, id(float_variable3), type(float_variable3),
+float_variable4, id(float_variable4), type(float_variable4),
+float_variable5, id(float_variable5), type(float_variable5))
+
+
+#STRING DATATYPE
+
+string_1 = "this is first string"
+string_2 = "this is second string"
+string_3 = """this is a multi line string.
+i love python"""
+string_4 = "i am tejaswini." \
+                "i just want to say hi to all." \
+                "we did'nt introduce our self."
+string_5 = "this is amazing language"
+
+print(string_1, type(string_1), id(string_1))
+print(string_2, type(string_2), id(string_2))
+print(string_3, type(string_3), id(string_3))
+print(string_4, type(string_4), id(string_4))
+print(string_5, type(string_5), id(string_5))
+
+
+#BOOLEAN DATATYPE
+
+true = True
+false = False
+
+true_1 = 1
+false_2 = 0
+
+print(true, type(true), id(true))
+print(false, type(false), id(false))
+print(true_1, type(true_1), id(true_1))
+print(false_2, type(false_2), id(false_2))
+
+
+#NONE DATATYPE
+
+none_datatype = None
+print(none_datatype, type(none_datatype), id(none_datatype))
+
+
+# 5 list variables
+
+
+
+
+
+
