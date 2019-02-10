c1b3c332d3bb741f2e2795ec08a589d2537b2dbc
chandrakant Patil <chandrakant.patil66@gmail.com>
2019-02-10 10:29:29 +0530

    hello word

---
+++ class03.py
@@ -0,0 +1,23 @@
+#Assignment
+
+#create dictionary of user data
+dict_03 = {
+    'Name': 'Chandrakant Patil',
+    'User name':  'chandrakant.patil66@gmail.com',
+    'Password': 123456789,
+    'Address': 'maji sainik nagar,Yerwada,pune-6,',
+    'Mobile No': 9637646900,
+    'Security Question': 'What is your Favourite Game'
+
+}
+print(dict_03)
+
+dic_04 = {
+    'Name': 'Tejaswini Nandagaonkar',
+    'User name': 'tejaswini22031994@gmail.com',
+    'Password': 54187181888,
+    'Address': 'vishrantwadi pune',
+    'Mobile No': 9036678400,
+}
+
+print(dic_04)
